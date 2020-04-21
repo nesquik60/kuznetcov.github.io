e8d6a363658b08179bb3c41a0e34bc38270372c3
nesquik60 <62826553+nesquik60@users.noreply.github.com>
2020-04-21 13:35:44 +0600

    Refactoring

--- adminPanel/js/sctipt.js
+++ adminPanel/js/sctipt.js
@@ -20,10 +20,42 @@ $('img.img-svg').each(function () {
   );
 });
 
+$('.header__burger-icon').click(function (e) {
+  $(
+    '.cross, .header__menu, .nav, .sidebar__link, .sidebar__btn, .menu-link, .sidebar__head'
+  ).addClass('active');
+});
 
-// $(".sidebar__link").click(function (e) {
-//     $(e.currentTarget)
-//       .find(".sidebar__link, svg.img-svg")
-//       .toggleClass("active")
-//       .end();
-//   });
+$('.cross').click(function (e) {
+  $(
+    '.cross, .header__menu, .nav, .sidebar__link, .sidebar__btn, .menu-link, .sidebar__head'
+  ).removeClass('active');
+});
+let color = ['#f39c13', '#00caff', '#5a51de', '#ff004f'];
+let hello = function () {
+  return Math.floor(Math.random() * color.length);
+};
+
+Object.defineProperty(Object.prototype, 'randElement', {
+  value: function () {
+    var rand = Math.floor(Math.random() * this.length);
+    return this[rand];
+  },
+});
+
+let line = $('.diving-line');
+let history = $('.fa-history');
+let arr = jQuery.makeArray(line);
+let arr1 = jQuery.makeArray(history);
+$(document).ready(function () {
+  for (var i = 0; i < arr.length; i++) {
+    $(arr.randElement()).css({ 'background-color': color[hello()] });
+    $(arr1.randElement()).css({ 'color': color[hello()] });
+  }
+});
+
+// for (var i = 0; i < arr.length; i++) {
+//   if (mas < arr) {
+//     mas.push(Math.floor(Math.random() * arr));
+//   }
+// }
