fb3f3a0a2f70f5337d3dfa3560b2587e807c9720
nesquik60 <62826553+nesquik60@users.noreply.github.com>
2020-05-04 21:25:50 +0600

    WP+Dev-Prod mode

--- landing_hotel/webpack.config.js
+++ landing_hotel/webpack.config.js
@@ -6,7 +6,7 @@ const PATHS = {
   build: path.join(__dirname, 'build'),
 };
 
-module.exports = {
+const common = {
   entry: {
     index: PATHS.source + '/pages/index/index.js',
     blog: PATHS.source + '/pages/blog/blog.js',
@@ -38,7 +38,18 @@ module.exports = {
       },
     ],
   },
+};
+const developmentConfig = {
   devServer: {
-    stats: 'errors-only'
+    stats: 'errors-only',
+    port: 9000,
   },
+};
+module.exports = function (env) {
+  if (env === 'production') {
+    return common;
+  }
+  if (env === 'development') {
+    return Object.assign({}, common, developmentConfig);
+  }
 };
